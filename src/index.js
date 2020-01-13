e4e37f85fc9eb181b60e4000d66f4cc7ed637684
DhungJoo Kim <dhungjoo.kim.19@dartmouth.edu>
2020-01-13 00:02:58 -0800

    created surge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import './style.scss';
+import $ from 'jquery';
+
+let num = 0;
+setInterval(() => {
+  num += 1;
+  $('#main').html(`Youve been on this page for ${num} seconds...`);
+}, 1000);
