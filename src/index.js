5e10808172cf1c158b218cf94762fdc5a1e08f9b
DJ Kim <djkim@minervaanalytics.co>
2020-01-13 16:26:23 -0500

    added react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './style.scss';
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let num = 0;
 setInterval(() => {
